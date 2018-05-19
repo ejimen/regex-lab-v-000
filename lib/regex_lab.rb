464acf2c1b34016b813044991306b24605384537
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-19 04:23:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ text.scan(/un+\w+ing/i)
 end
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
